6425cb385389f87ac42ccd3b9976e573169108bc
Igor Snitkin <snigo.ogins@gmail.com>
2022-08-18 00:23:30 +0200

    integrate contentful

---
+++ getContentfulEnvironment.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+const contentfulManagement = require('contentful-management');
+
+dotenv.config({ path: '.env.local' });
+
+module.exports = function () {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
+  });
+
+  return contentfulClient
+    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then(space => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT));
+};
